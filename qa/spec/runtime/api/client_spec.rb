964054c78f05757d700463d926de3231fb8087eb
Grant <chinomarino86@gmail.com>
2019-05-03 20:43:17 +0100

    refactoring unit tests for Runtime::API::Client to check new user variable

--- qa/spec/runtime/api/client_spec.rb
+++ qa/spec/runtime/api/client_spec.rb
@@ -14,25 +14,57 @@ describe QA::Runtime::API::Client do
   end
 
   describe '#personal_access_token' do
-    context 'when QA::Runtime::Env.personal_access_token is present' do
+    context 'when user is nil and QA::Runtime::Env.personal_access_token is present' do
       before do
         allow(QA::Runtime::Env).to receive(:personal_access_token).and_return('a_token')
       end
 
       it 'returns specified token from env' do
-        expect(described_class.new.personal_access_token).to eq 'a_token'
+        client = described_class.new
+
+        expect(client.personal_access_token).to eq 'a_token'
       end
     end
 
-    context 'when QA::Runtime::Env.personal_access_token is nil' do
+    context 'when user is present and QA::Runtime::Env.personal_access_token is nil' do
       before do
         allow(QA::Runtime::Env).to receive(:personal_access_token).and_return(nil)
       end
 
       it 'returns a created token' do
-        expect(subject).to receive(:create_personal_access_token).and_return('created_token')
+        client = described_class.new(user: { username: 'foo' })
 
-        expect(subject.personal_access_token).to eq 'created_token'
+        expect(client).to receive(:create_personal_access_token).and_return('created_token')
+
+        expect(client.personal_access_token).to eq 'created_token'
+      end
+    end
+
+    context 'when user is nil and QA::Runtime::Env.personal_access_token is nil' do
+      before do
+        allow(QA::Runtime::Env).to receive(:personal_access_token).and_return(nil)
+      end
+
+      it 'returns a created token' do
+        client = described_class.new
+
+        expect(client).to receive(:create_personal_access_token).and_return('created_token')
+
+        expect(client.personal_access_token).to eq 'created_token'
+      end
+    end
+
+    context 'when user is present and QA::Runtime::Env.personal_access_token is present' do
+      before do
+        allow(QA::Runtime::Env).to receive(:personal_access_token).and_return('a_token')
+      end
+
+      it 'returns a created token' do
+        client = described_class.new(user: { username: 'foo' })
+
+        expect(client).to receive(:create_personal_access_token).and_return('created_token')
+
+        expect(client.personal_access_token).to eq 'created_token'
       end
     end
   end
