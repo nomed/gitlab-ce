a3e252f54a0f9f68550ba2c5ed2a8d0694ab0cfe
Małgorzata Ksionek <meksionek@gmail.com>
2019-05-28 15:26:05 +0200

    Add issue stage events specs

--- spec/lib/gitlab/cycle_analytics/issue_stage_spec.rb
+++ spec/lib/gitlab/cycle_analytics/issue_stage_spec.rb
@@ -7,27 +7,35 @@ describe Gitlab::CycleAnalytics::IssueStage do
   let(:stage_name) { :issue }
 
   it_behaves_like 'base stage'
+  let(:project) { create(:project) }
+  let!(:issue_1) { create(:issue, project: project, created_at: 90.minutes.ago) }
+  let!(:issue_2) { create(:issue, project: project, created_at: 60.minutes.ago) }
+  let!(:issue_3) { create(:issue, project: project, created_at: 30.minute.ago) }
+  let!(:issue_without_milestone) { create(:issue, project: project, created_at: 1.minute.ago) }
+  let(:stage) { described_class.new(project: project, options: { from: 2.days.ago, current_user: project.creator }) }
+
+  before do
+    issue_1.metrics.update!(first_associated_with_milestone_at: 60.minutes.ago)
+    issue_2.metrics.update!(first_added_to_board_at: 30.minutes.ago)
+    issue_3.metrics.update!(first_added_to_board_at: 15.minutes.ago)
+  end
 
   describe '#median' do
-    let(:project) { create(:project) }
-    let!(:issue_1) { create(:issue, project: project, created_at: 90.minutes.ago) }
-    let!(:issue_2) { create(:issue, project: project, created_at: 60.minutes.ago) }
-    let!(:issue_3) { create(:issue, project: project, created_at: 30.minute.ago) }
-    let!(:issue_without_milestone) { create(:issue, project: project, created_at: 1.minute.ago) }
-    let(:stage) { described_class.new(project: project, options: { from: 2.days.ago }) }
-
-    before do
-      issue_1.metrics.update!(first_associated_with_milestone_at: 60.minutes.ago)
-      issue_2.metrics.update!(first_added_to_board_at: 30.minutes.ago)
-      issue_3.metrics.update!(first_added_to_board_at: 15.minutes.ago)
-    end
-
     around do |example|
       Timecop.freeze { example.run }
     end
 
     it 'counts median from issues with metrics' do
       expect(stage.median).to eq(HALF_AN_HOUR_IN_SECONDS)
+    end
+  end
+
+  describe '#events' do
+    it 'exposes issues with metrics' do
+      result = stage.events
+
+      expect(result.count).to eq(3)
+      expect(result.map{ |event| event[:title] }).to include(issue_1.title, issue_2.title, issue_3.title)
     end
   end
 end
