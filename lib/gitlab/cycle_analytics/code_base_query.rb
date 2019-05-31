827b6caa276c9edd0e7f2d69a7d832597389ed2c
Małgorzata Ksionek <mksionek@gitlab.com>
2019-05-31 15:00:01 +0200

    Modify code base query

--- lib/gitlab/cycle_analytics/code_base_query.rb
+++ lib/gitlab/cycle_analytics/code_base_query.rb
@@ -4,7 +4,7 @@ module Gitlab
   module CycleAnalytics
     module CodeBaseQuery
       def stage_query(project_ids)
-        super(project_ids).where(issue_metrics_table[:first_mentioned_in_commit_at].not_eq(nil))
+        super(project_ids).where(mr_table[:created_at].gteq(issue_metrics_table[:first_mentioned_in_commit_at]))
       end
     end
   end
