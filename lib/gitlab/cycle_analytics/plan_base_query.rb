ba0ebc68504b9f9728c047ea97dc4070a6db0e8c
Małgorzata Ksionek <mksionek@gitlab.com>
2019-06-01 18:08:50 +0200

    Fix multiple specs

--- lib/gitlab/cycle_analytics/plan_base_query.rb
+++ lib/gitlab/cycle_analytics/plan_base_query.rb
@@ -8,10 +8,10 @@ module Gitlab
           .project(issue_table[:project_id].as("project_id"))
           .where(issue_table[:project_id].in(project_ids))
           .where(issue_table[:created_at].gteq(@options[:from])) # rubocop:disable Gitlab/ModuleWithInstanceVariables
-          .where((issue_metrics_table[:first_added_to_board_at]).not_eq(nil).or(issue_metrics_table[:first_associated_with_milestone_at].not_eq(nil)))
+          .where(issue_metrics_table[:first_added_to_board_at].not_eq(nil).or(issue_metrics_table[:first_associated_with_milestone_at].not_eq(nil)))
           .where(issue_metrics_table[:first_mentioned_in_commit_at].not_eq(nil))
 
-          query
+        query
       end
     end
   end
