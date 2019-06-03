ece284daec1086187e150382566563596dcd9bbc
Nathan Friend <nathan@gitlab.com>
2019-06-03 15:16:05 -0300

    Improve pipeline status Slack notifications
    This commit adds some formatting to the Slack notifications for pipeline
    statuses, as well as adds information about the stage and jobs
    that failed in the case of pipeline failure.

--- app/models/project_services/chat_message/pipeline_message.rb
+++ app/models/project_services/chat_message/pipeline_message.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
+require 'slack-notifier'
 
 module ChatMessage
   class PipelineMessage < BaseMessage
+    MAX_VISIBLE_JOBS = 12
+
     attr_reader :ref_type
     attr_reader :ref
     attr_reader :status
+    attr_reader :detailed_status
     attr_reader :duration
+    attr_reader :finished_at
     attr_reader :pipeline_id
+    attr_reader :failed_stages
+    attr_reader :failed_jobs
+
+    attr_reader :project
+    attr_reader :commit
+    attr_reader :committer
+    attr_reader :pipeline
 
     def initialize(data)
       super
@@ -17,8 +29,17 @@ module ChatMessage
       @ref_type = pipeline_attributes[:tag] ? 'tag' : 'branch'
       @ref = pipeline_attributes[:ref]
       @status = pipeline_attributes[:status]
+      @detailed_status = pipeline_attributes[:detailed_status]
       @duration = pipeline_attributes[:duration].to_i
+      @finished_at = pipeline_attributes[:finished_at] ? Time.parse(pipeline_attributes[:finished_at]).to_i : nil
       @pipeline_id = pipeline_attributes[:id]
+      @failed_jobs = (data[:builds] || []).select { |b| b[:status] == 'failed' }.reverse # Show failed jobs from oldest to newest
+      @failed_stages = @failed_jobs.map { |j| j[:stage] }.uniq
+
+      @project = Project.find(data[:project][:id])
+      @commit = project.commit_by(oid: data[:commit][:id])
+      @committer = commit.committer
+      @pipeline = Ci::Pipeline.find(pipeline_id)
     end
 
     def pretext
@@ -28,14 +49,85 @@ module ChatMessage
     def attachments
       return message if markdown
 
-      [{ text: format(message), color: attachment_color }]
+      return [{ text: format(message), color: attachment_color }] unless Feature.enabled?(:fancy_pipeline_slack_notifications, default_enabled: true)
+
+      [{
+        fallback: format(message),
+        color: attachment_color,
+        author_name: user_combined_name,
+        author_icon: user_avatar,
+        author_link: author_url,
+        title: s_("SlackNotifications|Pipeline #%{pipeline_id} %{humanized_status} in %{duration}") %
+          {
+            pipeline_id: pipeline_id,
+            humanized_status: humanized_status,
+            duration: pretty_duration(duration)
+          },
+        title_link: pipeline_url,
+        fields: attachments_fields,
+        footer: project.name,
+        footer_icon: project.avatar_url,
+        ts: finished_at
+      }]
+    end
+
+    def failed_stages_field
+      {
+        title: s_("SlackNotifications|Failed stage").pluralize(failed_stages.length),
+        value: Slack::Notifier::LinkFormatter.format(failed_stages_links),
+        short: true
+      }
+    end
+
+    def failed_jobs_field
+      {
+        title: s_("SlackNotifications|Failed job").pluralize(failed_jobs.length),
+        value: Slack::Notifier::LinkFormatter.format(failed_jobs_links),
+        short: true
+      }
+    end
+
+    def yaml_error_field
+      {
+        title: s_("SlackNotifications|Invalid CI config YAML file"),
+        value: pipeline.yaml_errors,
+        short: false
+      }
+    end
+
+    def attachments_fields
+      fields = [
+        {
+          title: ref_type == "tag" ? s_("SlackNotifications|Tag") : s_("SlackNotifications|Branch"),
+          value: Slack::Notifier::LinkFormatter.format(ref_name_link),
+          short: true
+        },
+        {
+          title: s_("SlackNotifications|Commit"),
+          value: Slack::Notifier::LinkFormatter.format(commit_link),
+          short: true
+        }
+      ]
+
+      fields << failed_stages_field if failed_stages.any?
+      fields << failed_jobs_field if failed_jobs.any?
+      fields << yaml_error_field if pipeline.has_yaml_errors?
+
+      fields
     end
 
     def activity
       {
-        title: "Pipeline #{pipeline_link} of #{ref_type} #{branch_link} by #{user_combined_name} #{humanized_status}",
-        subtitle: "in #{project_link}",
-        text: "in #{pretty_duration(duration)}",
+        title: s_("SlackNotifications|Pipeline %{pipeline_link} of %{ref_type} %{branch_link} by %{user_combined_name} %{humanized_status}") %
+          {
+            pipeline_link: pipeline_link,
+            ref_type: ref_type,
+            branch_link: branch_link,
+            user_combined_name: user_combined_name,
+            humanized_status: humanized_status
+          },
+        subtitle: s_("SlackNotifications|in %{project_link}") % { project_link: project_link },
+        text: s_("SlackNotifications|in %{duration}") % { duration: pretty_duration(duration) },
         image: user_avatar || ''
       }
     end
@@ -43,36 +135,72 @@ module ChatMessage
     private
 
     def message
-      "#{project_link}: Pipeline #{pipeline_link} of #{ref_type} #{branch_link} by #{user_combined_name} #{humanized_status} in #{pretty_duration(duration)}"
+      s_("SlackNotifications|%{project_link}: Pipeline %{pipeline_link} of %{ref_type} %{branch_link} by %{user_combined_name} %{humanized_status} in %{duration}") %
+        {
+          project_link: project_link,
+          pipeline_link: pipeline_link,
+          ref_type: ref_type,
+          branch_link: branch_link,
+          user_combined_name: user_combined_name,
+          humanized_status: humanized_status,
+          duration: pretty_duration(duration)
+        }
     end
 
     def humanized_status
-      case status
-      when 'success'
-        'passed'
+      if Feature.enabled?(:fancy_pipeline_slack_notifications, default_enabled: true)
+        case status
+        when 'success'
+          detailed_status == "passed with warnings" ? s_("SlackNotifications|has passed with warnings") : s_("SlackNotifications|has passed")
+        when 'failed'
+          s_("SlackNotifications|has failed")
+        else
+          status
+        end
       else
-        status
+        case status
+        when 'success'
+          s_("SlackNotifications|passed")
+        when 'failed'
+          s_("SlackNotifications|failed")
+        else
+          status
+        end
       end
     end
 
     def attachment_color
-      if status == 'success'
-        'good'
+      if Feature.enabled?(:fancy_pipeline_slack_notifications, default_enabled: true)
+        case status
+        when 'success'
+          detailed_status == 'passed with warnings' ? 'warning' : 'good'
+        else
+          'danger'
+        end
       else
-        'danger'
+        case status
+        when 'success'
+          'good'
+        else
+          'danger'
+        end
       end
     end
 
     def branch_url
-      "#{project_url}/commits/#{ref}"
+      "#{project.web_url}/commits/#{ref}"
     end
 
     def branch_link
       "[#{ref}](#{branch_url})"
     end
 
+    def project_url
+      project.web_url
+    end
+
     def project_link
-      "[#{project_name}](#{project_url})"
+      "[#{project.name}](#{project_url})"
     end
 
     def pipeline_url
@@ -81,6 +209,69 @@ module ChatMessage
 
     def pipeline_link
       "[##{pipeline_id}](#{pipeline_url})"
+    end
+
+    def pipeline_jobs_url
+      "#{pipeline_url}/builds"
+    end
+
+    def job_url(job)
+      "#{project_url}/-/jobs/#{job[:id]}"
+    end
+
+    def job_link(job)
+      "[#{job[:name]}](#{job_url(job)})"
+    end
+
+    def failed_jobs_links
+      truncated_failed_jobs = failed_jobs
+      more_text = ""
+
+      # Instead of showing "and 1 more", we'll just show the final job
+      # We'll start showing "and x more" when the limit is exceeded by at least 2
+      if failed_jobs.length > MAX_VISIBLE_JOBS
+        truncated_failed_jobs = failed_jobs.take(MAX_VISIBLE_JOBS - 1)
+        more_text = s_("SlackNotifications|and [%{more_count} more](%{pipeline_jobs_url})") %
+          { more_count: failed_jobs.length - (MAX_VISIBLE_JOBS - 1), pipeline_jobs_url: pipeline_jobs_url }
+      end
+
+      links = truncated_failed_jobs.map { |j| job_link(j) }
+      links << more_text unless more_text.empty?
+
+      links.join(I18n.translate(:'support.array.words_connector'))
+    end
+
+    def stage_url(stage)
+      # All stages link to the pipeline page
+      pipeline_url
+    end
+
+    def stage_link(stage)
+      "[#{stage}](#{stage_url(stage)})"
+    end
+
+    def failed_stages_links
+      failed_stages.map { |s| stage_link(s) }.join(I18n.translate(:'support.array.words_connector'))
+    end
+
+    def commit_url
+      Gitlab::UrlBuilder.build(commit)
+    end
+
+    def commit_link
+      "[#{commit.title}](#{commit_url})"
+    end
+
+    def commits_page_url
+      "#{project_url}/commits/#{ref}"
+    end
+
+    def ref_name_link
+      "[#{ref}](#{commits_page_url})"
+    end
+
+    def author_url
+      committer ? Gitlab::UrlBuilder.build(committer) : nil
     end
   end
 end
--- spec/models/project_services/chat_message/pipeline_message_spec.rb
+++ spec/models/project_services/chat_message/pipeline_message_spec.rb
@@ -1,12 +1,56 @@
 # frozen_string_literal: true
-
 require 'spec_helper'
 
 describe ChatMessage::PipelineMessage do
   subject { described_class.new(args) }
 
-  let(:user) { { name: "The Hacker", username: 'hacker' } }
-  let(:duration) { 7210 }
+  before do
+    test_commit = double("A test commit", committer: user, title: "A test commit message")
+    test_project = double("A test project",
+                          commit_by: test_commit, name: project[:name],
+                          web_url: project[:web_url], avatar_url: project[:avatar_url])
+    allow(Project).to receive(:find) { test_project }
+
+    test_pipeline = double("A test pipeline", has_yaml_errors?: has_yaml_errors,
+                          yaml_errors: "yaml error description here")
+    allow(Ci::Pipeline).to receive(:find) { test_pipeline }
+
+    allow(Gitlab::UrlBuilder).to receive(:build).with(test_commit).and_return("http://example.com/commit")
+    allow(Gitlab::UrlBuilder).to receive(:build).with(user).and_return("http://example.gitlab.com/hacker")
+  end
+
+  let(:user) do
+    {
+      id: 345,
+      name: "The Hacker",
+      username: "hacker",
+      email: "hacker@example.gitlab.com",
+      avatar_url: "http://example.com/avatar"
+    }
+  end
+
+  let(:name_of_user) { user ? "#{user[:name]} (#{user[:username]})" : "API" }
+
+  let(:user_profile_link) { user ? "http://example.gitlab.com/hacker" : nil }
+
+  let(:project) do
+    {
+      id: 234,
+      name: "project_name",
+      path_with_namespace: 'group/project_name',
+      web_url: 'http://example.gitlab.com',
+      avatar_url: 'http://example.com/project_avatar'
+    }
+  end
+
+  let(:status) { "success" }
+
+  let(:detailed_status) { nil }
+
+  let(:has_yaml_errors) { false }
+
+  let(:builds) { nil }
+
   let(:args) do
     {
       object_attributes: {
@@ -15,58 +59,240 @@ describe ChatMessage::PipelineMessage do
         tag: false,
         ref: 'develop',
         status: status,
-        duration: duration
+        detailed_status: detailed_status,
+        duration: 7210,
+        finished_at: "2019-05-27 11:56:36 -0300"
       },
-      project: {
-        path_with_namespace: 'project_name',
-        web_url: 'http://example.gitlab.com'
+      project: project,
+      user: user,
+      commit: {
+        id: "abcdef"
       },
-      user: user
+      builds: builds
     }
   end
-  let(:combined_name) { "The Hacker (hacker)" }
+
+  let(:slack_formatted_message) do
+    "<http://example.gitlab.com|project_name>:" \
+        " Pipeline <http://example.gitlab.com/pipelines/123|#123>" \
+        " of branch <http://example.gitlab.com/commits/develop|develop>" \
+        " by #{name_of_user} #{status_text} in 02:00:10"
+  end
+
+  let(:markdown_message) do
+    "[project_name](http://example.gitlab.com):" \
+        " Pipeline [#123](http://example.gitlab.com/pipelines/123)" \
+        " of branch [develop](http://example.gitlab.com/commits/develop)" \
+        " by #{name_of_user} #{status_text} in 02:00:10"
+  end
+
+  let(:activity) do
+    {
+      title: "Pipeline [#123](http://example.gitlab.com/pipelines/123) of branch [develop](http://example.gitlab.com/commits/develop) by #{name_of_user} #{status_text}",
+      subtitle: "in [project_name](http://example.gitlab.com)",
+      text: 'in 02:00:10',
+      image: user ? user[:avatar_url] : ""
+    }
+  end
+
+  let(:simple_attachments) do
+    [{
+      text: slack_formatted_message,
+      color: color
+    }]
+  end
+
+  let(:additional_fields) { [] }
+
+  let(:fancy_attachments) do
+    [{
+      fallback: slack_formatted_message,
+      color: color,
+      author_name: name_of_user,
+      author_icon: user ? user[:avatar_url] : nil,
+      author_link: user_profile_link,
+      title: "Pipeline #123 #{status_text} in 02:00:10",
+      title_link: "http://example.gitlab.com/pipelines/123",
+      fields: [
+        {
+          title: "Branch",
+          value: "<http://example.gitlab.com/commits/develop|develop>",
+          short: true
+        },
+        {
+          title: "Commit",
+          value: "<http://example.com/commit|A test commit message>",
+          short: true
+        }
+      ].concat(additional_fields),
+      footer: "project_name",
+      footer_icon: "http://example.com/project_avatar",
+      ts: 1558968996
+    }]
+  end
 
   context 'without markdown' do
+    shared_examples 'a correct response' do
+      context 'when the fancy_pipeline_slack_notifications feature flag is disabled' do
+        let(:status_text) { status == "success" ? "passed" : status }
+        let(:color) { status == "success" ? "good" : "danger" }
+
+        it 'returns the correct pretext, fallback, and attachments' do
+          stub_feature_flags(fancy_pipeline_slack_notifications: false)
+
+          expect(subject.pretext).to be_empty
+          expect(subject.fallback).to eq(slack_formatted_message)
+          expect(subject.attachments).to eq(simple_attachments)
+        end
+      end
+
+      context 'when the fancy_pipeline_slack_notifications feature flag is enabled' do
+        let(:status_text) do
+          case status
+          when "success"
+            detailed_status == "passed with warnings" ? "has passed with warnings" : "has passed"
+          else
+            "has failed"
+          end
+        end
+        let(:color) do
+          case status
+          when "success"
+            detailed_status == "passed with warnings" ? "warning" : "good"
+          else
+            "danger"
+          end
+        end
+
+        it 'returns the correct pretext, fallback, and attachments' do
+          stub_feature_flags(fancy_pipeline_slack_notifications: true)
+
+          expect(subject.pretext).to be_empty
+          expect(subject.fallback).to eq(slack_formatted_message)
+          expect(subject.attachments).to eq(fancy_attachments)
+        end
+      end
+    end
+
     context 'pipeline succeeded' do
       let(:status) { 'success' }
-      let(:color) { 'good' }
-      let(:message) { build_message('passed', combined_name) }
 
-      it 'returns a message with information about succeeded build' do
-        expect(subject.pretext).to be_empty
-        expect(subject.fallback).to eq(message)
-        expect(subject.attachments).to eq([text: message, color: color])
+      it_behaves_like 'a correct response'
+
+      context 'with warnings' do
+        let(:detailed_status) { 'passed with warnings' }
+
+        it_behaves_like 'a correct response'
       end
     end
 
     context 'pipeline failed' do
       let(:status) { 'failed' }
-      let(:color) { 'danger' }
-      let(:message) { build_message(status, combined_name) }
 
-      it 'returns a message with information about failed build' do
-        expect(subject.pretext).to be_empty
-        expect(subject.fallback).to eq(message)
-        expect(subject.attachments).to eq([text: message, color: color])
+      context 'when a single job fails' do
+        let(:builds) do
+          [
+            { id: 567, name: "rspec", status: "failed", stage: "test" },
+            { id: 678, name: "karma", status: "success", stage: "test" }
+          ]
+        end
+        let(:additional_fields) do
+          [
+            { title: "Failed stage", value: "<http://example.gitlab.com/pipelines/123|test>", short: true },
+            { title: "Failed job", value: "<http://example.gitlab.com/-/jobs/567|rspec>", short: true }
+          ]
+        end
+
+        it_behaves_like 'a correct response'
+      end
+
+      context 'when two jobs fail' do
+        let(:builds) do
+          [
+            { id: 567, name: "rspec", status: "failed", stage: "test" },
+            { id: 678, name: "eslint", status: "failed", stage: "test" },
+            { id: 789, name: "karma", status: "success", stage: "test" }
+          ]
+        end
+        let(:additional_fields) do
+          [
+            { title: "Failed stage", value: "<http://example.gitlab.com/pipelines/123|test>", short: true },
+            { title: "Failed jobs", value: "<http://example.gitlab.com/-/jobs/678|eslint>, <http://example.gitlab.com/-/jobs/567|rspec>", short: true }
+          ]
+        end
+
+        it_behaves_like 'a correct response'
+      end
+
+      context 'when 12 jobs fail' do
+        let(:builds) do
+          (1..ChatMessage::PipelineMessage::MAX_VISIBLE_JOBS).map do |i|
+            { id: i, name: "failed-job-#{i}", status: "failed", stage: "test" }
+          end
+        end
+        let(:additional_fields) do
+          failed_jobs_text_array = builds.reverse.map { |b| "<http://example.gitlab.com/-/jobs/#{b[:id]}|#{b[:name]}>" }
+
+          [
+            { title: "Failed stage", value: "<http://example.gitlab.com/pipelines/123|test>", short: true },
+            { title: "Failed jobs", value: failed_jobs_text_array.join(', '), short: true }
+          ]
+        end
+
+        it_behaves_like 'a correct response'
+      end
+
+      context 'when 13 jobs fail' do
+        let(:builds) do
+          (1..ChatMessage::PipelineMessage::MAX_VISIBLE_JOBS + 1).map do |i|
+            { id: i, name: "failed-job-#{i}", status: "failed", stage: "test" }
+          end
+        end
+        let(:additional_fields) do
+          failed_jobs_text_array = builds.reverse.first(ChatMessage::PipelineMessage::MAX_VISIBLE_JOBS - 1).map { |b| "<http://example.gitlab.com/-/jobs/#{b[:id]}|#{b[:name]}>" }
+          failed_jobs_text_array.push("and <http://example.gitlab.com/pipelines/123/builds|2 more>")
+
+          [
+            { title: "Failed stage", value: "<http://example.gitlab.com/pipelines/123|test>", short: true },
+            { title: "Failed jobs", value: failed_jobs_text_array.join(', '), short: true }
+          ]
+        end
+
+        it_behaves_like 'a correct response'
+      end
+
+      context 'when jobs fail in multiple stages' do
+        let(:builds) do
+          [
+            { id: 567, name: "rspec", status: "failed", stage: "test" },
+            { id: 678, name: "karma", status: "success", stage: "test" },
+            { id: 789, name: "review", status: "failed", stage: "review" }
+          ]
+        end
+        let(:additional_fields) do
+          [
+            { title: "Failed stages", value: "<http://example.gitlab.com/pipelines/123|review>, <http://example.gitlab.com/pipelines/123|test>", short: true },
+            { title: "Failed jobs", value: "<http://example.gitlab.com/-/jobs/789|review>, <http://example.gitlab.com/-/jobs/567|rspec>", short: true }
+          ]
+        end
+
+        it_behaves_like 'a correct response'
+      end
+
+      context 'when the yaml file is invalid' do
+        let(:has_yaml_errors) { true }
+        let(:additional_fields) do
+          [{ title: "Invalid CI config YAML file", value: "yaml error description here", short: false }]
+        end
+
+        it_behaves_like 'a correct response'
       end
 
       context 'when triggered by API therefore lacking user' do
         let(:user) { nil }
-        let(:message) { build_message(status, 'API') }
 
-        it 'returns a message stating it is by API' do
-          expect(subject.pretext).to be_empty
-          expect(subject.fallback).to eq(message)
-          expect(subject.attachments).to eq([text: message, color: color])
-        end
+        it_behaves_like 'a correct response'
       end
-    end
-
-    def build_message(status_text = status, name = user[:name])
-      "<http://example.gitlab.com|project_name>:" \
-        " Pipeline <http://example.gitlab.com/pipelines/123|#123>" \
-        " of branch <http://example.gitlab.com/commits/develop|develop>" \
-        " by #{name} #{status_text} in 02:00:10"
     end
   end
 
@@ -75,61 +301,64 @@ describe ChatMessage::PipelineMessage do
       args[:markdown] = true
     end
 
-    context 'pipeline succeeded' do
-      let(:status) { 'success' }
-      let(:color) { 'good' }
-      let(:message) { build_markdown_message('passed', combined_name) }
+    shared_examples 'a correct response' do
+      context 'when the fancy_pipeline_slack_notifications feature flag is disabled' do
+        let(:status_text) { status == "success" ? "passed" : status }
+        let(:color) { status == "success" ? "good" : "danger" }
 
-      it 'returns a message with information about succeeded build' do
-        expect(subject.pretext).to be_empty
-        expect(subject.attachments).to eq(message)
-        expect(subject.activity).to eq({
-          title: 'Pipeline [#123](http://example.gitlab.com/pipelines/123) of branch [develop](http://example.gitlab.com/commits/develop) by The Hacker (hacker) passed',
-          subtitle: 'in [project_name](http://example.gitlab.com)',
-          text: 'in 02:00:10',
-          image: ''
-        })
-      end
-    end
+        it 'returns the correct pretext, attachments, and activity' do
+          stub_feature_flags(fancy_pipeline_slack_notifications: false)
 
-    context 'pipeline failed' do
-      let(:status) { 'failed' }
-      let(:color) { 'danger' }
-      let(:message) { build_markdown_message(status, combined_name) }
-
-      it 'returns a message with information about failed build' do
-        expect(subject.pretext).to be_empty
-        expect(subject.attachments).to eq(message)
-        expect(subject.activity).to eq({
-          title: 'Pipeline [#123](http://example.gitlab.com/pipelines/123) of branch [develop](http://example.gitlab.com/commits/develop) by The Hacker (hacker) failed',
-          subtitle: 'in [project_name](http://example.gitlab.com)',
-          text: 'in 02:00:10',
-          image: ''
-        })
-      end
-
-      context 'when triggered by API therefore lacking user' do
-        let(:user) { nil }
-        let(:message) { build_markdown_message(status, 'API') }
-
-        it 'returns a message stating it is by API' do
           expect(subject.pretext).to be_empty
-          expect(subject.attachments).to eq(message)
-          expect(subject.activity).to eq({
-            title: 'Pipeline [#123](http://example.gitlab.com/pipelines/123) of branch [develop](http://example.gitlab.com/commits/develop) by API failed',
-            subtitle: 'in [project_name](http://example.gitlab.com)',
-            text: 'in 02:00:10',
-            image: ''
-          })
+          expect(subject.attachments).to eq(markdown_message)
+          expect(subject.activity).to eq(activity)
+        end
+      end
+
+      context 'when the fancy_pipeline_slack_notifications feature flag is enabled' do
+        let(:status_text) do
+          case status
+          when "success"
+            detailed_status == "passed with warnings" ? "has passed with warnings" : "has passed"
+          else
+            "has failed"
+          end
+        end
+        let(:color) do
+          case status
+          when "success"
+            detailed_status == "passed with warnings" ? "warning" : "good"
+          else
+            "danger"
+          end
+        end
+
+        it 'returns the correct pretext, attachments, and activity' do
+          stub_feature_flags(fancy_pipeline_slack_notifications: true)
+
+          expect(subject.pretext).to be_empty
+          expect(subject.attachments).to eq(markdown_message)
+          expect(subject.activity).to eq(activity)
         end
       end
     end
 
-    def build_markdown_message(status_text = status, name = user[:name])
-      "[project_name](http://example.gitlab.com):" \
-        " Pipeline [#123](http://example.gitlab.com/pipelines/123)" \
-        " of branch [develop](http://example.gitlab.com/commits/develop)" \
-        " by #{name} #{status_text} in 02:00:10"
+    context 'pipeline succeeded' do
+      let(:status) { 'success' }
+
+      it_behaves_like 'a correct response'
+    end
+
+    context 'pipeline failed' do
+      let(:status) { 'failed' }
+
+      it_behaves_like 'a correct response'
+
+      context 'when triggered by API therefore lacking user' do
+        let(:user) { nil }
+
+        it_behaves_like 'a correct response'
+      end
     end
   end
 end
