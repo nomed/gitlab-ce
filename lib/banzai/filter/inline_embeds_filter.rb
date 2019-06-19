2c2e643f7a1ed5cd6eb6781da6fac6f617b61762
syasonik <syasonik@gitlab.com>
2019-06-19 19:38:04 +0300

    Expose metrics element for FE consumption
    Adds GFM Pipline filters to insert a placeholder in the generated
    HTML from GFM based on the presence of a metrics dashboard link.
    
    The front end should look for the class 'js-render-metrics' to
    determine if it should replace the element with metrics charts.
    The data element 'data-dashboard-url' should be the endpoint
    the front end should hit in order to obtain a dashboard layout
    in order to appropriately render the charts.

---
+++ lib/banzai/filter/inline_embeds_filter.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Banzai
+  module Filter
+    # HTML filter that inserts a node for each occurence of
+    # a given link format. To transform references to DB
+    # resources in place, prefer to inherit from AbstractReferenceFilter.
+    class InlineEmbedsFilter < HTML::Pipeline::Filter
+      # Find every relevant link, create a new node based on
+      # the link, and insert this node after any html content
+      # surrounding the link.
+      def call
+        doc.xpath(xpath_search).each do |node|
+          next unless params = embed_params(node)
+
+          element = element_to_embed(doc, params)
+
+          next unless element
+          
+          # We want this to follow any surrounding content. For example,
+          # if a link is inline in a paragraph.
+          node.parent.children.last.add_next_sibling(element)
+        end
+
+        doc
+      end
+
+      # Implement in child class.
+      #
+      # Return a Nokogiri::XML::Element to embed in the
+      # markdown.
+      def element_to_embed(doc, params)
+      end
+
+      # Implement in child class unless overriding #embed_params
+      #
+      # Returns the regex pattern used to filter
+      # to only matching urls.
+      def url_regex
+      end
+
+      # Returns the query string used to select nodes
+      # from the html document on which the embed is based.
+      #
+      # Override to select nodes other than links.
+      def xpath_search
+        'descendant-or-self::a[@href]'
+      end
+
+      # Returns a hash of named parameters based on the
+      # provided regex with string keys.
+      #
+      # Override to select nodes other than links.
+      def embed_params(node)
+        url = node['href']
+
+        url_regex.match(url) { |m| m.named_captures }
+      end
+    end
+  end
+end
---
+++ lib/banzai/filter/inline_metrics_filter.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# Generated HTML is transformed back to GFM by app/assets/javascripts/behaviors/markdown/nodes/image.js
+module Banzai
+  module Filter
+    # HTML filter that inserts a placeholder element for each
+    # reference to a metrics dashboard.
+    class InlineMetricsFilter < Banzai::Filter::InlineEmbedsFilter
+      # Placeholder element for the frontend to use as an
+      # injection point for charts.
+      def element_to_embed(doc, params)
+        doc.document.create_element(
+          'div',
+          class: 'js-render-metrics',
+          'data-dashboard-url': embedded_metrics_url(params),
+          'data-environment': params['environment'],
+          'data-project': params['project'],
+          'data-namespace': params['namespace'],
+        )
+      end
+
+      # Endpoint FE should hit to collect the appropriate
+      # chart information
+      def embedded_metrics_url(params)
+        Gitlab::Routing.url_helpers.metrics_dashboard_namespace_project_environment_url(
+          params['namespace'],
+          params['project'],
+          params['environment'],
+          embedded: true
+        )
+      end
+
+      # Identifies the url for the metrics dashboard.
+      # https://<host>/<namespace>/<project>/environments/<env_id>/metrics
+      def url_regex
+        %r{
+          (?<url>
+            #{Regexp.escape(Gitlab.config.gitlab.url)}
+            \/#{Project.reference_pattern}
+            (?:\/\-)?
+            \/environments
+            \/(?<environment>\d+)
+            \/metrics
+            (?<path>
+              (\/[a-z0-9_=-]+)*
+            )?
+            (?<query>
+              \?[a-z0-9_=-]+
+              (&[a-z0-9_=-]+)*
+            )?
+            (?<anchor>\#[a-z0-9_-]+)?
+          )
+        }x
+      end
+    end
+  end
+end
